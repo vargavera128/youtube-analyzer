a7343b09d3c2dadd0a6e5527c553a43967dd0066
Varga Veronika <varga.veronika@hallgato.sze.hu>
2025-05-09 15:31:26 +0100

    INIT - index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+require('dotenv').config();
+const Fastify = require('fastify');
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+    console.log('Server running on http://localhost:3000');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
