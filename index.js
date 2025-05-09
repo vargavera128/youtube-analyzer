5792bb89b4b53604ff2ac07ea266e28ad3685a55
Varga Veronika <varga.veronika@hallgato.sze.hu>
2025-05-09 18:19:11 +0100

    UPDATE - db connect

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 require('dotenv').config();
 const fastify = require('fastify')({ logger: true });
 const youtubeRoutes = require('./routes/youtube.routes');
+const { knex } = require("./database");
 
-fastify.register(youtubeRoutes);
 
-fastify.listen({ port: 5000 }, (err, address) => {
+
+const start = async () => { 
+  for (;;) {
+    try {
+      await knex.raw("select 1+1");
+      break;
+    } catch (err) {
+      console.log(err);
+      console.log("Unable to connect to PGSQL, waiting...");
+      await new Promise((resolve) => setTimeout(resolve, 1000)); 
+    }
+  }
+  
+  let version = await knex.raw(
+    `SELECT default_version, installed_version FROM pg_available_extensions where name = 'timescaledb'`
+  );
+  if (!version) {
+    console.log("ERROR: TimescaleDB is not installed on the DB instance");
+    process.exit(1);
+  }
+
+  await knex.migrate.latest({ directory: "dbmigrations" }); 
+
+  fastify.register(youtubeRoutes);
+
+
+  fastify.listen({ port: 5000 }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
   fastify.log.info(`Server listening at ${address}`);
 });
+};
+
+start();
+module.exports = fastify;
+
+
