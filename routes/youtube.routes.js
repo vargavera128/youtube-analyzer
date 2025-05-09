3ac07babddf7fe8715e9becbca0d206b3adc9080
Varga Veronika <varga.veronika@hallgato.sze.hu>
2025-05-09 18:21:19 +0100

    UPDATE - getVideoMetaData

--- routes/youtube.routes.js
+++ routes/youtube.routes.js
@@ -1,36 +1,26 @@
 const { analyzeComments } = require('../services/sentiment.service');
-const { getComments } = require('../services/youtube.service');
+const { getComments, getVideoMetadata } = require('../services/youtube.service');
 
-async function routes(fastify, options) {
-  fastify.get('/analyze-comments', async (request, reply) => {
-    const { videoId } = request.query;
+async function youtubeRoutes(fastify, options) {
+fastify.get('/analyze-comments', async (request, reply) => {
+  const { videoId } = request.query;
 
-    if (!videoId) {
-      return reply.code(400).send({ error: 'Missing videoId parameter' });
-    }
+  if (!videoId) {
+    return reply.code(400).send({ error: 'Missing videoId parameter' });
+  }
 
-    try {
-      const comments = await getComments(videoId);
+  try {
+    const comments = await getComments(videoId);
+    const videoMetadata = await getVideoMetadata(videoId);
 
-      const analysisResults = await analyzeComments(comments);
+    const analysisResults = await analyzeComments(comments, videoId, videoMetadata);
 
-      const summary = analysisResults.reduce( // TODO: javítani
-        (acc, curr) => {
-          acc[curr.sentiment]++;
-          return acc;
-        },
-        { pozitív: 0, negatív: 0 }
-      );
-
-      return {
-        summary,
-        results: analysisResults,
-      };
-    } catch (err) {
-      console.error(err);
-      return reply.code(500).send({ error: 'Error during analysis' });
-    }
-  });
+    return reply.send(analysisResults);
+  } catch (err) {
+    fastify.log.error(err);
+    return reply.code(500).send({ error: 'Error during analysis' });
+  }
+});
 }
 
-module.exports = routes;
+module.exports = youtubeRoutes;
